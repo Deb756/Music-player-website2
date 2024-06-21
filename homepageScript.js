c5479b0e4d1114e0c26a32b07051e740486cf745
Debabrat <debabratasenapati2021@gmail.com>
2024-06-21 20:15:24 +0530

    Initial commit

---
+++ homepageScript.js
@@ -0,0 +1,5 @@
+let nava = document.getElementsByClassName("nav-item");
+console.log(nava);
+let nava2 = addEventListener("click", () => {
+    document.querySelectorAll(".nav-link").classList.add("active")
+})
